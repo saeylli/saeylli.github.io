b82abec58908c4cb3f0f5addc45c54d91a9f6234
saeylli <146027550+saeylli@users.noreply.github.com>
2023-12-11 13:38:29 -0500

    Add files via upload

--- s3fp/sketch.js
+++ s3fp/sketch.js
@@ -1,8 +1,8 @@
 let x, y, minX, maxX, minY, maxY;
-let rectColor; // Variable for the rectangle's color
-let greenRectColor; // Color for all green rectangles
+let sandColor; // Variable for the rectangle's color
+let plantColor; // Color for all green rectangles
 let fishColor; // Color for all fish shapes
-let greenRects = []; // Array to store green rectangles
+let plants = []; // Array to store green rectangles
 let fishes = []; // Array to store fish shapes
 
 // Gradient rectangle dimensions
@@ -16,25 +16,27 @@ function setup() {
   x = width / 2;
   y = height / 2;
 
-  // Define boundaries for stepper movement
+  // boundaries for sand
   minX = gradX;
   maxX = gradX + gradW - 15;
   minY = gradY + 2 * gradH / 3;
   maxY = gradY + gradH - 15;
 
-  // Set background and gradient
+  // Set background 
   background(188, 184, 177);
 
+  // set gradient color
   let color1 = color(random(0, 255), random(0, 255), random(0, 255));
   let color2 = color(random(0, 255), random(0, 255), random(0, 255));
   setGradient(gradX, gradY, gradW, gradH, color1, color2, "Y");
   
-  // Additional static elements
+  // static elements
   drawStaticElements();
 
-  // Set random colors for shapes
-  rectColor = color(random(0, 255), random(0, 255), random(0, 255));
-  // greenRectColor = color(random(0, 255), random(0, 255), random(0, 255));
+  // random set color for sand
+  sandColor = color(random(0, 255), random(0, 255), random(0, 255));
+
+  // random set color for fish
   fishColor = color(random(0, 255), random(0, 255), random(0, 255)); // Random color for all fish shapes
 
 }
@@ -44,7 +46,8 @@ function resetSketch() {
 }
 
 function draw() {
-  // Draw fish shapes
+  
+  // Draw fishes
   fill(fishColor);
   for (let fish of fishes) {
     let fishX = constrain(fish.x, gradX - 25, gradX + gradW - 55);
@@ -67,70 +70,54 @@ function draw() {
     pop(); // Restore the original drawing state
   }
   
-  // Draw green rectangles
-  
+  // Draw plants of varying lengths
   fill(170, 203, 77);
-for (let greenRect of greenRects) {
-  let rectX = constrain(greenRect.x, gradX, gradX + gradW - 20);
-  let rectWidth = 20; // Width of the rectangle
-  let rectHeight = constrain(greenRect.height, 20, gradH);
-  let rectY = gradY + gradH - rectHeight - 15; // Y-position of the rectangle
+  for (let plant of plants) {
+    let rectX = constrain(plant.x, gradX, gradX + gradW - 20);
+    let rectWidth = 20; // Width of the rectangle
+    let rectHeight = constrain(plant.height, 20, gradH);
+    let rectY = gradY + gradH - rectHeight - 15; // Y-position of the rectangle
 
-  // Draw the rectangle
-  rect(rectX, rectY, rectWidth, rectHeight);
+    // draws rectangles
+    rect(rectX, rectY, rectWidth, rectHeight);
 
-  // Draw a semicircle on top of the rectangle
-  arc(rectX + rectWidth / 2, rectY, rectWidth, rectWidth, PI, 0);
+    // draws a semicircle on top of the rectangle to make it look more plant-y
+    arc(rectX + rectWidth / 2, rectY, rectWidth, rectWidth, PI, 0);
   
-  let lineStartY = rectY + rectWidth / 2; // Starting just below the semicircle
-  let lineEndY = rectY + rectHeight; // Ending at the bottom of the rectangle
-  stroke(95, 179, 37); // Set line color (black)
-  strokeWeight(2);
-  line(rectX + rectWidth / 2, lineStartY, rectX + rectWidth / 2, lineEndY);
-  noStroke(); // Reset stroke to avoid affecting other elements 
-}
+    // draws a line for the plant to make it look "leafy"
+    let lineStartY = rectY + rectWidth / 2; // Starting just below the semicircle
+    let lineEndY = rectY + rectHeight; // Ending at the bottom of the rectangle
+    stroke(95, 179, 37); // Set line color (black)
+    strokeWeight(2);
+    line(rectX + rectWidth / 2, lineStartY, rectX + rectWidth / 2, lineEndY);
+    noStroke(); // Reset stroke to avoid affecting other elements 
+  }
 
 
 
-  // Draw interactive steppers above everything
+  // draws sand particles when mouse is clicked (and dragged)
   if (mouseIsPressed) {
-    // Update stepper position and constrain within the bottom third
+    // Update sand particle position and constrain within the bottom one-third of the gradient (water)
     x = constrain(mouseX, minX, maxX);
     y = constrain(mouseY, minY, maxY);
-    fill(rectColor);
+    fill(sandColor);
     rect(x, y, 15, 15); // Draw the stepper last to ensure it's on top
   }
   
-  stroke(299);
-  strokeWeight(2);
-  line(60, 130, 300, 400);
-  line(125, 150, 365, 420);
-  line(450, 140, 690, 400);
-  line(550, 140, 605, 200);
-  noStroke();
-  
-  fill(245, 235, 224);
-  rect(750, 417, 90, 55, 25);
-  rect(750, 370, 90, 55, 25);
-  rect(750, 323, 90, 55, 25);
-  
-  fill(252, 191, 73);
-  ellipse(795,300, 25, 40);
-  
-  fill(188, 184, 177);
-  rect(792, 310, 5, 20, 25);
   
 }
 
+// checks for user input
 function keyPressed() {
+  // space creates plants at random locations
   if (key === ' ') {
     // Add new green rectangle
-    greenRects.push({
+    plants.push({
       x: random(gradX, gradX + gradW - 20),
       height: random(20, gradH - 40)
     });
   } else {
-    // Add a new fish shape with random direction
+    // other keys creates a new fish shape with random direction
     fishes.push({
       x: random(gradX + 50, gradX + gradW - 30),
       y: random(gradY + 15, gradY + gradH - 30),
@@ -139,6 +126,7 @@ function keyPressed() {
   }
 }
 
+// makes gradient from two randomized colors
 function setGradient(x, y, w, h, c1, c2, axis) {
   noFill();
   if (axis === "Y") {
@@ -150,6 +138,7 @@ function setGradient(x, y, w, h, c1, c2, axis) {
     }
   }
 }
+
 
 function drawStaticElements() {
   // Table
@@ -168,4 +157,25 @@ function drawStaticElements() {
   noStroke();
   fill(19, 41, 61);
   rect(20, 85, 720, 35, 8);
+
+  // glass texture
+  stroke(299);
+  strokeWeight(2);
+  line(60, 130, 300, 400);
+  line(125, 150, 365, 420);
+  line(450, 140, 690, 400);
+  line(550, 140, 605, 200);
+  noStroke();
+  
+  // draws candle on table
+  fill(245, 235, 224);
+  rect(750, 417, 90, 55, 25);
+  rect(750, 370, 90, 55, 25);
+  rect(750, 323, 90, 55, 25);
+  
+  fill(252, 191, 73);
+  ellipse(795,300, 25, 40);
+  
+  fill(188, 184, 177);
+  rect(792, 310, 5, 20, 25);
 }
